20aad1f766b86c7a111964e689ab50c03948b4c7
Junglenaunt <tmaxmhala@gmail.com>
2025-03-14 14:03:03 +0700

    Cập nhật cấu trúc form, đổi Form1 thành MainForm

---
+++ Task Master/MainForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task_Master
+{
+    public partial class MainForm : Form
+    {
+        private FlowLayoutPanel boardPanel;
+        private Button addListButton;
+        private List<Panel> listPanels = new List<Panel>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetupUI();
+        }
+        private void SetupUI()
+        {
+            this.Text = "Task Master";
+            this.Size = new Size(1000, 600);
+
+            boardPanel = new FlowLayoutPanel()
+            {
+                Dock = DockStyle.Fill,
+                AutoScroll = true,
+                FlowDirection = FlowDirection.LeftToRight,
+                WrapContents = false
+            };
+            this.Controls.Add(boardPanel);
+
+            addListButton = new Button()
+            {
+                Text = "+ Thêm danh sách",
+                Width = 150,
+                Height = 40
+            };
+            addListButton.Click += AddListButton_Click;
+            boardPanel.Controls.Add(addListButton);
+        }
+
+        private void AddListButton_Click(object sender, EventArgs e)
+        {
+            Panel listPanel = CreateListPanel("New List");
+            boardPanel.Controls.Add(listPanel);
+            boardPanel.Controls.SetChildIndex(addListButton, boardPanel.Controls.Count - 1);
+            listPanels.Add(listPanel);
+        }
+
+        private Panel CreateListPanel(string title)
+        {
+            Panel panel = new Panel()
+            {
+                Width = 240,
+                Height = 400,
+                BackColor = Color.LightGray,
+                Padding = new Padding(5)
+            };
+
+            TextBox titleBox = new TextBox()
+            {
+                Text = title,
+                Width = 180,
+                Font = new Font("Arial", 10, FontStyle.Bold),
+                TextAlign = HorizontalAlignment.Center,
+                Location = new Point(10, 5)
+            };
+            panel.Controls.Add(titleBox);
+
+            Button deleteButton = new Button()
+            {
+                Text = "X",
+                Width = 30,
+                Height = 30,
+                Location = new Point(panel.Width - 40, 5),
+                BackColor = Color.Red,
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat
+            };
+
+            deleteButton.Click += (s, e) =>
+            {
+                DialogResult result = MessageBox.Show(
+                    "Bạn có chắc chắn muốn xóa danh sách này?",
+                    "Xác nhận xóa",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    boardPanel.Controls.Remove(panel);
+                    listPanels.Remove(panel);
+                }
+            };
+
+            panel.Controls.Add(deleteButton);
+
+            return panel;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
